29e823f0b603fce1aae48a7b1af9f848b7b87604
lolhesap <62035111+lolhesap@users.noreply.github.com>
2021-03-08 04:56:06 +0300

    Update setup.bat

--- val/setup.bat
+++ val/setup.bat
@@ -1,6 +1,6 @@
 @echo off
 set mypath=%~dp0
 echo Installing...
-"%mypath%/install.exe" /install
+"%mypath%/driver_installer.exe" /install
 echo Installation is complete, please restart the computer.
 pause
