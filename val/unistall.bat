864ecaf78820b90b4622fb0187390681358115d3
lolhesap <62035111+lolhesap@users.noreply.github.com>
2021-03-08 04:55:57 +0300

    Update unistall.bat

--- val/unistall.bat
+++ val/unistall.bat
@@ -1,6 +1,6 @@
 @echo off
 set mypath=%~dp0
 echo Installing...
-"%mypath%/install.exe" /uninstall
+"%mypath%/driver_installer.exe" /uninstall
 echo uninstallation is complete, please restart the computer.
 pause
